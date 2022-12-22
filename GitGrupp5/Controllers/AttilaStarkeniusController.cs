42b0bb4ccfd442fa77065453d09d872fe5f954ea
Attila Starkenius <symphtom@hotmail.com>
2022-12-22 13:11:48 +0100

    22.12.2022. Attila Starkenius Adding AttilaStarkeniusController.cs

---
+++ GitGrupp5/Controllers/AttilaStarkeniusController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitGrupp5.Controllers
+{
+    public class AttilaStarkeniusController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
